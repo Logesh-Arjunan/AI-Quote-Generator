197a3bade46609c0cca280551ff39045b401fda2
Logesh Arjunan <alogesh1220052@gmail.com>
2026-08-01 14:02:34 +0530

    Add server/models/User.js

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true },
+  lastLogin: { type: Date, default: Date.now },
+  stats: {
+    totalChats: { type: Number, default: 0 },
+    totalQuotes: { type: Number, default: 0 },
+    favoriteCategory: { type: String, default: "Motivation" },
+    favoriteMood: { type: String, default: "Happy" },
+    longestChat: { type: Number, default: 0 },
+    avgResponseTime: { type: Number, default: 0 }
+  }
+}, { timestamps: true });
+
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
